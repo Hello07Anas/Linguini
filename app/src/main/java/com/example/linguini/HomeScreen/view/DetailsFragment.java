093c2bfe336f291b41298f33f54d3a82ed228e82
Anas Salah <120333186+Hello07Anas@users.noreply.github.com>
2024-02-16 14:42:12 +0200

    adding vido to detailsView and created gif for coming soon on not completed views

--- app/src/main/java/com/example/linguini/HomeScreen/view/DetailsFragment.java
+++ app/src/main/java/com/example/linguini/HomeScreen/view/DetailsFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,20 +25,27 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.util.List;
+
 public class DetailsFragment extends Fragment implements DetailsView {
     private String mealId;
     private DetailsPresenterIMP detailsPresenterIMP;
+    String videoId;
+
+    ImageView imgGif;
+
+    Button btnPlay;
     private static final String TAG = "DetailsFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         detailsPresenterIMP = new DetailsPresenterIMP(this, MealsRepoIMP.getInstance(
                 MealsRemoteDataSourceIMP.getInstance(getContext())
         ));
-
         mealId = DetailsFragmentArgs.fromBundle(getArguments()).getSelectedMeal();
-        Log.i(TAG, "onCreate: " + mealId);
+        //Log.i(TAG, "onCreate: " + mealId);
     }
 
     @Override
@@ -45,57 +53,53 @@ public class DetailsFragment extends Fragment implements DetailsView {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_details, container, false);
         detailsPresenterIMP.getMealDetails(mealId);
-
-        YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
-        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-            @Override
-            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                super.onReady(youTubePlayer);
-                Log.i(TAG, "YouTube Player is ready.");
-            }
-
-            @Override
-            public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError error) {
-                super.onError(youTubePlayer, error);
-                Log.e(TAG, "YouTube Player Error: " + error.toString());
-            }
-        });
-
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
         // Any additional initialization code can go here
     }
 
-
-
-
-
     @Override
     public void showDetailse(MealResponse mealResponse) {
-        Log.i(TAG, "show Meal: " + mealResponse.getMealDay().get(0).getMealName());
+        //Log.i(TAG, "show Meal: " + mealResponse.getMealDay().get(0).getMealName());
 
         // Retrieve meal details from the response
         PojoForMeal pojoForMeal = mealResponse.getMealDay().get(0);
 
+        // Log the image URL
+        //Log.i(TAG, "Image URL: " + pojoForMeal.getImageUrl());
+
         // Populate UI elements with meal data
         TextView mealNameTextView = getView().findViewById(R.id.meal_name);
         TextView mealCountryTextView = getView().findViewById(R.id.meal_country);
+        TextView txtInstructions = getView().findViewById(R.id.txtInstructions);
         ImageView mealImageView = getView().findViewById(R.id.meal_image);
 
         mealNameTextView.setText(pojoForMeal.getMealName());
         mealCountryTextView.setText(pojoForMeal.getAreaOfMeal());
+
+
         Glide.with(getContext()).load(pojoForMeal.getImageUrl())
                 .placeholder(R.drawable.logo2)
                 .error(R.drawable.palastine)
                 .into(mealImageView);
 
+        imgGif = getView().findViewById(R.id.imgGif);
+        Glide.with(requireContext())
+                .asGif() // Specify that the resource is a GIF
+                .load(R.drawable.mlaha) // Load the GIF from resources
+                .placeholder(R.drawable.palastine) // Placeholder image while loading
+                .error(R.drawable.palastine) // Error image if loading fails
+                .into(imgGif); // Set the GIF to the ImageView
+
         // Extract video ID from the YouTube URL
         String videoUrl = pojoForMeal.getStrYoutube();
-        String videoId = getId(videoUrl);
+        videoId = extractVideoIdFromUrl(videoUrl);
         Log.i(TAG, "showDetailse: " + videoId);
         // Initialize YouTubePlayerView and load the video
         YouTubePlayerView youTubePlayerView = getView().findViewById(R.id.youtube_player_view);
@@ -104,16 +108,50 @@ public class DetailsFragment extends Fragment implements DetailsView {
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
             public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                youTubePlayer.loadVideo(videoId, 0);
+                youTubePlayer.cueVideo(videoId, 0);
+                Log.i(TAG, "onReady: " + videoId);
+                //youTubePlayer.loadVideo(videoId.substring(32),0);
             }
-
             @Override
             public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError error) {
                 super.onError(youTubePlayer, error);
                 Log.e(TAG, "YouTube Player Error: " + error.toString());
             }
         });
+
+// Set ingredients
+        List<String> ingredients = pojoForMeal.getIngredients();
+
+// Ensure that the ingredients list is not null
+        if (ingredients != null) {
+            // Log the number of ingredients
+            Log.i(TAG, "Number of Ingredients: " + ingredients.size());
+
+            // Loop through the first 12 ingredients and populate TextViews
+            for (int i = 0; i < Math.min(ingredients.size(), 12); i++) {
+                // Find the TextView for the ingredient using its ID
+                int ingredientTextViewId = getResources().getIdentifier("strIngredient" + (i + 1), "id", requireContext().getPackageName());
+                TextView ingredientTextView = getView().findViewById(ingredientTextViewId);
+
+                // Log the ingredient being set
+                Log.d(TAG, "Setting Ingredient " + (i + 1) + ": " + ingredients.get(i));
+
+                // Ensure that the TextView is found
+                if (ingredientTextView != null) {
+                    ingredientTextView.setText(ingredients.get(i));
+                } else {
+                    // Log if TextView is not found
+                    Log.e(TAG, "Ingredient TextView not found for index: " + (i + 1));
+                }
+            }
+        } else {
+            // Log if ingredients list is null
+            Log.e(TAG, "Ingredients list is null");
+        }
     }
+
+
+
 
     @Override
     public void showDetailseErrorMSG(String error) {
@@ -125,184 +163,21 @@ public class DetailsFragment extends Fragment implements DetailsView {
         // Implement your logic here for handling clicks on meal items
     }
 
-    // Utility method to extract video ID from YouTube URL
-
-    public String getId(String link) {
-        if (link != null && link.split("\\?v=").length > 1)
-            return link.split("\\?v=")[1];
-        else return "";
+    private String extractVideoIdFromUrl(String youtubeUrl) {
+        String videoId = null;
+        if (youtubeUrl != null && youtubeUrl.trim().length() > 0 && youtubeUrl.startsWith("https://www.youtube.com/")) {
+            String[] urlParts = youtubeUrl.split("v=");
+            if (urlParts.length > 1) {
+                videoId = urlParts[1];
+                int ampersandPosition = videoId.indexOf('&');
+                if (ampersandPosition != -1) {
+                    videoId = videoId.substring(0, ampersandPosition);
+                }
+            }
+        }
+        return videoId;
     }
-//    private String extractVideoIdFromUrl(String youtubeUrl) {
-//        String videoId = null;
-//        if (youtubeUrl != null && youtubeUrl.trim().length() > 0 && youtubeUrl.startsWith("https://www.youtube.com/")) {
-//            String[] urlParts = youtubeUrl.split("v=");
-//            if (urlParts.length > 1) {
-//                videoId = urlParts[1];
-//                int ampersandPosition = videoId.indexOf('&');
-//                if (ampersandPosition != -1) {
-//                    videoId = videoId.substring(0, ampersandPosition);
-//                }
-//            }
-//        }
-//        return videoId;
-//    }
 }
 
 
 
-
-
-//package com.example.linguini.HomeScreen.view;
-//
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.fragment.app.Fragment;
-//
-//import com.bumptech.glide.Glide;
-//import com.example.linguini.HomeScreen.model.Pojos.Single.PojoForMeal;
-//import com.example.linguini.HomeScreen.model.Pojos.Response.MealResponse;
-//import com.example.linguini.HomeScreen.model.network.MealService;
-//import com.example.linguini.HomeScreen.model.network.MealsRemoteDataSource;
-//import com.example.linguini.HomeScreen.model.network.MealsRemoteDataSourceIMP;
-//import com.example.linguini.HomeScreen.model.repo.MealsRepoIMP;
-//import com.example.linguini.HomeScreen.presenter.DetailsPresenterIMP;
-//import com.example.linguini.HomeScreen.presenter.HomePresenter;
-//import com.example.linguini.HomeScreen.presenter.HomePresenterIMP;
-//import com.example.linguini.R;
-//import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
-//import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-//import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-//import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
-////import com.squareup.picasso.Picasso;
-//
-//import retrofit2.Call;
-//import retrofit2.Callback;
-//import retrofit2.Response;
-//import retrofit2.Retrofit;
-//import retrofit2.converter.gson.GsonConverterFactory;
-//
-//public class DetailsFragment extends Fragment implements DetailsView {
-//    private String mealId;
-//    DetailsPresenterIMP detailsPresenterIMP;
-//    private static final String TAG = "DetailsFragment";
-//
-//
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        detailsPresenterIMP = new DetailsPresenterIMP(this, MealsRepoIMP.getInstance(
-//                MealsRemoteDataSourceIMP.getInstance(getContext())
-//        ));
-//
-//
-//        mealId = DetailsFragmentArgs.fromBundle(getArguments()).getSelectedMeal();
-//        Log.i(TAG, "onCreate: " + mealId);
-//
-//    }
-//
-//
-//
-//
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_details, container, false);
-//
-//    }
-//
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//        detailsPresenterIMP.getMealDetails(mealId);
-//
-//
-//
-//        // TODO <<<  find by id
-//
-//    }
-//
-//
-//    @Override
-//    public void showDetailse(MealResponse mealResponse) {
-//        Log.i(TAG, "show Meal: " + mealResponse.getMealDay().get(0).getMealName());
-//
-//        // Retrieve meal details from the response
-//        PojoForMeal pojoForMeal = mealResponse.getMealDay().get(0); // Assuming mealDay is a list containing meals, adjust this accordingly
-//
-//        // Populate UI elements with meal data
-//        TextView mealNameTextView = getView().findViewById(R.id.meal_name);
-//        TextView mealCountryTextView = getView().findViewById(R.id.meal_country);
-//        ImageView mealImageView = getView().findViewById(R.id.meal_image);
-//
-//        mealNameTextView.setText(pojoForMeal.getMealName());
-//        mealCountryTextView.setText(pojoForMeal.getAreaOfMeal());
-//
-//        Glide.with(getContext()).load(pojoForMeal.getImageUrl())
-//                .placeholder(R.drawable.logo2)
-//                .error(R.drawable.palastine)
-//                .into(mealImageView);
-//
-//        String videoUrl = pojoForMeal.getStrYoutube();
-//        Log.d(TAG, "YouTube Video URL: " + videoUrl);
-//        String videoId = extractVideoIdFromUrl(videoUrl);
-//        Log.d(TAG, "Extracted Video ID: " + videoId);
-//
-//
-//        YouTubePlayerView youTubePlayerView = getView().findViewById(R.id.youtube_player_view);
-//        getLifecycle().addObserver(youTubePlayerView);
-//
-//        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-//            @Override
-//            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-//                youTubePlayer.loadVideo(videoId, 0);
-//            }
-//
-//            @Override
-//            public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError error) {
-//                super.onError(youTubePlayer, error);
-//                Log.e(TAG, "YouTube Player Error: " + error.toString());
-//            }
-//        });
-//    }
-//
-//
-//    @Override
-//    public void showDetailseErrorMSG(String error) {
-//        Log.i(TAG, "showArea: " + error);
-//    }
-//
-//    @Override
-//    public void onClicked(PojoForMeal pojoForMeal, View view) {
-//
-//    }
-//
-//
-//// Youtube video <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//    private String extractVideoIdFromUrl(String youtubeUrl) {
-//        String videoId = null;
-//        if (youtubeUrl != null && youtubeUrl.trim().length() > 0 && youtubeUrl.startsWith("https://www.youtube.com/")) {
-//            String[] urlParts = youtubeUrl.split("v=");
-//            if (urlParts.length > 1) {
-//                videoId = urlParts[1];
-//                int ampersandPosition = videoId.indexOf('&');
-//                if (ampersandPosition != -1) {
-//                    videoId = videoId.substring(0, ampersandPosition);
-//                }
-//            }
-//        }
-//        return videoId;
-//    }
-//
-//}
--- app/src/main/java/com/example/linguini/HomeScreen/view/FavoritesFragment.java
+++ app/src/main/java/com/example/linguini/HomeScreen/view/FavoritesFragment.java
@@ -2,16 +2,22 @@ package com.example.linguini.HomeScreen.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.linguini.R;
 
 
 public class FavoritesFragment extends Fragment {
+
+    ImageView imgCommingSoon2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,20 @@ public class FavoritesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imgCommingSoon2 = requireView().findViewById(R.id.imgCommingSoon2);
+        Glide.with(requireContext())
+                .asGif() // Specify that the resource is a GIF
+                .load(R.drawable.comming_soon) // Load the GIF from resources
+                .placeholder(R.drawable.palastine) // Placeholder image while loading
+                .error(R.drawable.palastine) // Error image if loading fails
+                .into(imgCommingSoon2); // Set the GIF to the ImageView
+    }
+
 }
 
 
